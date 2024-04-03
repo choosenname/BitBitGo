c6a9fa65546158ff449d2b2096aae854b3c5be50
choosenname <vlad@luckashonok.ru>
2024-04-03 14:23:45 +0300

    feat: implement greeting

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BitBitGo</title>
+</head>
+<body>
+    <?php include './header.html'; ?>
+    <?php include './greeting.html'; ?>
+</body>
+</html>
