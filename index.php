f418a7a515c24d1f8d769692d09c8a8663bd7cef
choosenname <vlad@luckashonok.ru>
2024-04-04 13:42:10 +0300

    feat: add faq page

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>BitBitGo</title>
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
@@ -18,5 +18,8 @@
     <?php include './s5-lessons.html'; ?>
     <?php include './s6-curriculum.html'; ?>
     <?php include './s7-second-layer.html'; ?>
+    <?php include './s8-price.html'; ?>
+    <?php include './s9-booking-form.html'; ?>
+    <?php include './s10-faq.html'; ?>
 </body>
 </html>
